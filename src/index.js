2b59c87240362e34a7c0419a5ced1dde08bf8e52
unknown <olga1gordeyeva@gmail.com>
2019-10-31 18:40:20 -0400

    fetch astronomy image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 
-console.log("Hello world");
+const getAPIRes = async (url) => {
+    const res = await fetch(url);
+    const resultApi = await res.json();
+    return resultApi;
+};
+
+getAPIRes('https://api.nasa.gov/planetary/apod?api_key=yLdrlHcI879YU86fgDcpwxbUBHkbh5tobw26qvyF')
+    .then((resultApi) => {
+        console.log(resultApi);
+    });
+
