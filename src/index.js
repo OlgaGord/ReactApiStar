cb3e89cdddbe9cd7350489d470b67bc2aabb9656
unknown <olga1gordeyeva@gmail.com>
2019-11-25 13:01:44 -0500

    last version of api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 const getAPIRes = async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Can not fetch ${url}` +
+            `error ${res.status}`)
+    }
+
+
     const resultApi = await res.json();
     return resultApi;
 };
 
-getAPIRes('https://api.nasa.gov/planetary/apod?api_key=yLdrlHcI879YU86fgDcpwxbUBHkbh5tobw26qvyF')
+getAPIRes('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=yLdrlHcI879YU86fgDcpwxbUBHkbh5tobw26qvyF')
     .then((resultApi) => {
         console.log(resultApi);
-    });
+    })
+    .catch((error) => {
+        console.error("Can not fetch", error);
+    })
 
