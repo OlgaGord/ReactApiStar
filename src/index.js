6e486957bccfc77a4782fdc7e817b6b68bad346a
unknown <olga1gordeyeva@gmail.com>
2019-11-25 22:53:05 -0500

    add picture of the day

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const getResDayPic = async (url2) => {
+    const resResPicDay = await fetch(url2);
+    if (!resResPicDay.ok) {
+        throw new Error(`Can not fetch ${url2}` +
+            `error ${resResPicDay.status}`)
+    }
+
+
+    const resultApiDayPic = await resResPicDay.json();
+    return resultApiDayPic;
+};
+getResDayPic('https://api.nasa.gov/planetary/apod?api_key=yLdrlHcI879YU86fgDcpwxbUBHkbh5tobw26qvyF')
+    .then((resultApiDayPic) => {
+        console.log(resultApiDayPic);
+    })
+    .catch((error) => {
+        console.error("Can not fetch", error);
+    })
 
 const getAPIRes = async (url) => {
     const res = await fetch(url);
